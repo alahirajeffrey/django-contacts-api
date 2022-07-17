3653e4b7fc8168b250cbfd911297e6648037a6c6
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-17 02:48:53 +0100

    setup database models

--- contacts/contacts_api/models.py
+++ contacts/contacts_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name: models.CharField(max_length=50)
+    last_name: models.CharField(max_length=50)
+    nickname: models.CharField(max_length=50)
+    email: models.CharField(max_length=50, unique=True)
+    phone_number: models.IntegerField(unique=True)
+    created_at: models.DateField()
+
+    def __str__(self):
+        contact = {"name": self.name, "number": self.phone_number}
+        return contact
