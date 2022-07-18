4d08faa971b150849879e1c05b38ec3bf525b3ae
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-18 11:51:43 +0100

    added apps to installed app

--- contacts/contacts/settings.py
+++ contacts/contacts/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contacts_api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
