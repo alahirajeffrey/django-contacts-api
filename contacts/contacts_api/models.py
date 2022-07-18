0533d2a215f7321c26a429e6cb1e7d28abf9543c
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-18 12:31:20 +0100

    fixed error with model

--- contacts/contacts_api/models.py
+++ contacts/contacts_api/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    first_name: models.CharField(max_length=50)
-    last_name: models.CharField(max_length=50)
-    nickname: models.CharField(max_length=50)
-    email: models.CharField(max_length=50, unique=True)
-    phone_number: models.IntegerField(unique=True)
-    created_at: models.DateField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
+    phone_number = models.IntegerField(unique=True)
+    created_at = models.DateField(auto_now=True)
 
     def __str__(self):
         contact = {"name": self.name, "number": self.phone_number}
